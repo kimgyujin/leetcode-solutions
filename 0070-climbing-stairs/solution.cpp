2cc90536432cf30760abcf630e98ed45973f9cf0
kimgyujin <37271003+kimgyujin@users.noreply.github.com>
2024-09-26 02:09:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (74.50%)

--- 0070-climbing-stairs/solution.cpp
+++ 0070-climbing-stairs/solution.cpp
@@ -1,7 +1,10 @@
 class Solution {
-    long long int f[46];
 public:
     int climbStairs(int n) {
+		    // 각 층로부터 1층 또는 2층으로 나갈 수 있기 때문에 점화식을 다음과 같이 세울 수 있음.
+		    // a_i = a_(i-1) + a_(i-2)
+		    // 사실 피보나치 수열
+		    vector <long long int> f(n+1);
         f[0] = 1;
         f[1] = 1;
         for(int i=2;i<=n;i++)
